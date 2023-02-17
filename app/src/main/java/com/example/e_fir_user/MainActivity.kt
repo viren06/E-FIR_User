0085bbe41090c180190108b35bc7129c9ea7ea51
virenpatel <viren000p@gmail.com>
2023-02-17 17:00:26 +0530

    login , forgot password done, started working on option menu and dashboard.

--- app/src/main/java/com/example/e_fir_user/MainActivity.kt
+++ app/src/main/java/com/example/e_fir_user/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.e_fir_user
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val zoom_in= AnimationUtils.loadAnimation(this,R.anim.zoom_in)
+        val imageView= findViewById(R.id.imageView) as ImageView
+        imageView.startAnimation(zoom_in)
+
+        val zoom_logo= AnimationUtils.loadAnimation(this,R.anim.zoom_logo)
+        val imagelogo= findViewById(R.id.imagelogo) as ImageView
+        imagelogo.startAnimation(zoom_logo)
+
+
+
+        Handler().postDelayed({
+
+            startActivity(Intent(this@MainActivity,user_login::class.java))
+            finish()
+        },3000)
     }
 }
--- app/src/main/java/com/example/e_fir_user/user_login.kt
+++ app/src/main/java/com/example/e_fir_user/user_login.kt
@@ -1,11 +1,91 @@
 package com.example.e_fir_user
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
 
 class user_login : AppCompatActivity() {
+    override fun onStart() {
+        super.onStart()
+        var user = FirebaseAuth.getInstance().currentUser
+        if (user != null) {
+            updateUi(user)
+        }
+    }
+    private var mAuth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_login)
+
+        mAuth=FirebaseAuth.getInstance()
+        val user = mAuth!!.currentUser
+
+        var email=findViewById<EditText>(R.id.textView1)
+        var password=findViewById<EditText>(R.id.textView2)
+
+        val login=findViewById<Button>(R.id.button)
+
+        val account_btn=findViewById<Button>(R.id.button_register)
+        account_btn.setOnClickListener {
+            startActivity(Intent(this,register_user::class.java))
+        }
+
+        val forgetpassword=findViewById<TextView>(R.id.lblforgetPass)
+        forgetpassword.setOnClickListener {
+            startActivity(Intent(this,forgot_password::class.java))
+            Toast.makeText(this,"diracting to forgot password page",Toast.LENGTH_LONG).show()
+        }
+
+//        if(user != null){
+//            updateUi(user)
+//        }
+
+        login.setOnClickListener{
+
+            val database = FirebaseDatabase.getInstance()
+            val myRef = database.getReference("User")
+            var progressDialog = ProgressDialog(this)
+            progressDialog.setTitle("It will take some time")
+            progressDialog.setMessage("hello")
+            progressDialog.show()
+
+
+            mAuth!!.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if(task.isSuccessful){
+                        mAuth=FirebaseAuth.getInstance()
+                        val user = mAuth!!.currentUser
+                        updateUi(user)
+                        progressDialog.dismiss()
+                    }
+
+                    else {
+                        // If sign in fails, display a message to the user.
+                        //Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+    }
+    private fun updateUi(user: FirebaseUser?) {
+        if(user!!.isEmailVerified){
+            startActivity(Intent(this,dashboard::class.java))
+            finish()
+        }
+        else{
+            user.sendEmailVerification()
+            Toast.makeText(this,"Verify email first", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
