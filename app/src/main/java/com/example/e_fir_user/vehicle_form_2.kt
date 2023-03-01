f1783c2349677d306e9b5f4fbc75016a8beff969
manthanjani <83779661+manthanjani@users.noreply.github.com>
2023-03-01 16:46:21 +0530

    In the "Forgot Password Activity" please try to change the text color in the textfield to accent. I tried but layout changes with color.

---
+++ app/src/main/java/com/example/e_fir_user/vehicle_form_2.kt
@@ -0,0 +1,11 @@
+package com.example.e_fir_user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class vehicle_form_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vehicle_form2)
+    }
+}
